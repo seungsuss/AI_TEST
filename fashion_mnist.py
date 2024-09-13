7a3f809e51e47f16ddb822e00c71800f1cd55232
seungsuss <seungsu1.yi@gmail.com>
2024-09-14 01:37:43 +0900

    "code change"

---
+++ fashion_mnist.py
@@ -0,0 +1,246 @@
+import torch 
+import torch.nn as nn 
+from torchvision import datasets , transforms
+from torch.utils.data import random_split,DataLoader,Subset
+from torchmetrics import Accuracy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import torch.nn as nn
+import os
+
+def check_device(str):
+    if str == "mac" :
+        device="mps" if torch.backends.mps.is_available() else "cpu"
+    else :
+        device="cuda" if torch.cuda.is_available() else "cpu"
+
+    return device
+
+def mnist_init(show_dataset =  1):
+    print("---------------------------------------------")
+    print("--            FASHION MNIST MODEL          --")
+    print("--                    program date: 2024.9 --")
+    print("--                         program by odin --")
+    print("---------------------------------------------")
+
+    device = check_device("mac")
+
+
+    
+
+    transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,),(0.5,))
+    ]) 
+
+    traindf=datasets.FashionMNIST(root="/Users/seungsulee/Documents/dataset",download=True,train=True,transform=transform)
+    testdf=datasets.FashionMNIST(root="/Users/seungsulee/Documents/dataset",download=True,train=False,transform=transform)
+
+    traindf[0][0].max(),traindf[0][0].min()
+
+    
+
+    idx=np.random.randint(0,len(traindf),25)
+    sample=Subset(traindf,idx)
+    label_classes=traindf.classes
+
+
+    traindf,valdf=random_split(traindf,[len(traindf)-10000,10000])
+    len(traindf),len(valdf)
+
+    trainloader=DataLoader(traindf,64,shuffle=True)
+    testloader=DataLoader(testdf,64,shuffle=True)
+    valloader=DataLoader(valdf,64,shuffle=True)
+
+    plt.figure(figsize=(10,8))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.imshow(sample[i][0].squeeze(),cmap="gray")
+        plt.title(label_classes[sample[i][1]])
+        plt.axis("off")
+    
+    plt.tight_layout()
+    plt.show()
+
+    return device , trainloader, testloader , valloader
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()        
+        
+        self.model = nn.Sequential(
+            nn.Linear(28*28, 128),
+            nn.LeakyReLU(),        # LeakyReLU activation function after the first layer
+            
+            # Second Linear layer: input is 128 features, output is 64 features
+            nn.Linear(128, 64),
+            nn.LeakyReLU(),        # LeakyReLU activation function after the second layer
+            
+            # Third Linear layer: input is 64 features, output is 10 classes (for FashionMNIST)
+            nn.Linear(64, 10)
+        )
+    
+    def forward(self, x):
+        # Flatten the input tensor to be of shape (batch_size, 28*28)
+        x = x.view(-1, 28*28)
+        
+        # Pass the flattened input through the defined model layers
+        return self.model(x)
+    
+
+def train(model, trainloader, loss_fn, optimizer, metric, device):
+    # Initialize a list to store the loss for each batch
+    total_loss = []
+    
+    # Set the model to training mode (important for layers like dropout or batch norm)
+    model.train()
+    
+    # Loop over each batch of images and labels from the DataLoader
+    for images, labels in trainloader:
+        # Move the images and labels to the specified device (CPU or GPU)
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Clear the gradients from the previous step to avoid accumulation
+        optimizer.zero_grad()
+        
+        # Forward pass: Get model predictions for the current batch of images
+        preds = model(images)
+        
+        # Calculate the loss between the predictions and the actual labels
+        loss = loss_fn(preds, labels)
+        
+        # Backward pass: Compute gradients for the model's parameters
+        loss.backward()
+        
+        # Update the model's parameters using the optimizer
+        optimizer.step()
+        
+        # Record the loss for this batch
+        total_loss.append(loss.item())
+
+        metric.update(preds, labels)
+        
+    # Calculate the average loss over all batches
+    avg_loss = np.mean(total_loss)
+    
+    # Compute the overall accuracy for the entire epoch
+    avg_acc = metric.compute().item()
+    
+    # Reset the metric state for the next epoch
+    metric.reset()
+    
+    # Return the average loss and accuracy, rounded to four decimal places
+    return round(avg_loss, 4), round(100 * avg_acc, 4)
+
+def validate(model, vailloader, loss_fn, metric, device):
+    total_loss = []
+    model.eval()  # Set the model to evaluation mode
+    
+    with torch.no_grad():  # Disable gradient calculations
+        for images, labels in vailloader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            # Forward pass to get predictions
+            preds = model(images)
+            
+            # Calculate loss between predictions and true labels
+            lss = loss_fn(preds, labels)
+            total_loss.append(lss.item()) 
+            
+            # Update the metric with current batch predictions and labels
+            metric.update(preds, labels)
+    
+    # Calculate the average loss over all batches
+    avg_loss = np.mean(total_loss)
+    
+    # Compute the overall accuracy
+    avg_acc = metric.compute().item()
+    
+    metric.reset()
+    return round(avg_loss, 4), round(100 * avg_acc, 4)
+
+def set_loss_func(model, device):
+    loss=nn.CrossEntropyLoss()
+    opt=torch.optim.Adam(model.parameters(),lr=0.001)
+    metric=Accuracy(task="multiclass",num_classes=10).to(device)
+
+    return loss, opt, metric
+
+def mnist_model_proc(mode , epoch_num = 0):
+
+    device , trainloader, testloader , valloader = mnist_init()
+
+    mymodel=Model()
+    mymodel.to(device)
+    mymodel(torch.rand(10,28,28).to(device)).shape
+
+    #loss=nn.CrossEntropyLoss()
+    #opt=torch.optim.Adam(mymodel.parameters(),lr=0.001)
+    #metric=Accuracy(task="multiclass",num_classes=10).to(device)
+   
+    loss, opt, metric = set_loss_func(mymodel, device)
+
+    if os.path.isfile("model_save/mnist_model.pth"):
+        mymodel = torch.load("model_save/mnist_model.pth")
+
+    if mode == 0 : ## train
+        train(mymodel, trainloader, loss, opt, metric, device) # check
+        validate(mymodel, valloader, loss, metric, device)    
+        
+        for i in range(epoch_num):
+            avg_loss_train, avg_acc_train = train(mymodel, trainloader, loss, opt, metric, device)
+            avg_loss_test, avg_acc_test = validate(mymodel, valloader, loss, metric, device)
+    
+            print(f"For epoch {i+1}, the training loss is {avg_loss_train} and accuracy is {avg_acc_train}%, "
+              f"and for validation, loss is {avg_loss_test} and accuracy is {avg_acc_test}%.")
+        
+
+    torch.save(mymodel,"model_save/mnist_model.pth") 
+
+
+
+
+if __name__ == '__main__':
+
+    device , trainloader, testloader , valloader = mnist_init()
+
+    mymodel=Model()
+    mymodel.to(device)
+    mymodel(torch.rand(10,28,28).to(device)).shape
+
+    #loss=nn.CrossEntropyLoss()
+    #opt=torch.optim.Adam(mymodel.parameters(),lr=0.001)
+    #metric=Accuracy(task="multiclass",num_classes=10).to(device)
+   
+    loss, opt, metric = set_loss_func(mymodel, device)
+
+    if os.path.isfile("model_save/mnist_model.pth"):
+        mymodel = torch.load("model_save/mnist_model.pth")
+
+    train(mymodel, trainloader, loss, opt, metric, device) # check
+    validate(mymodel, valloader, loss, metric, device)    
+
+    num_epochs = 10
+    for i in range(num_epochs):
+        avg_loss_train, avg_acc_train = train(mymodel, trainloader, loss, opt, metric, device)
+        avg_loss_test, avg_acc_test = validate(mymodel, valloader, loss, metric, device)
+    
+        print(f"For epoch {i+1}, the training loss is {avg_loss_train} and accuracy is {avg_acc_train}%, "
+              f"and for validation, loss is {avg_loss_test} and accuracy is {avg_acc_test}%.")
+        
+
+    torch.save(mymodel,"model_save/mnist_model.pth")
+    
+
+
+
+    
+
+
+    
+    
+    
