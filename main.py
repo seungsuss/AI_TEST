f2594e652093e45a828f3f8319a0365a855b8b0d
seungsuss <seungsu1.yi@gmail.com>
2024-09-14 01:38:50 +0900

    code change(main)

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("MAIN PY CODE")
+import sys
+from fashion_mnist import mnist_model_proc as mnist_model
+
+if __name__ == '__main__':
+    print("---------------------------------------------")
+    print("--            AI MODEL Program             --")
+    print("--                    program date: 2024.9 --")
+    print("--                         program by odin --")
+    print("---------------------------------------------")    
+    
+    if sys.argv[1] == '-h':
+        print('arg : -r(run), -m(view moel)')
+        print("python3 -r [Model] [train :0 , inference :1 ] epoch_count")
+    
+    if sys.argv[1] == '-m':
+        print("1. fashion mnist")
+
+    if sys.argv[1] == '-r':
+        if sys.argv[2] == '1' : ## mnist model
+            if sys.argv[3] == '0' : ## train    
+                print("----mnist train")            
+                mnist_model(0, int(sys.argv[4]))
+    
+
+
