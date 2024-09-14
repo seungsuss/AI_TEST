0f697299825f664b1e708cea25f236eef0291fa2
seungsuss <seungsu1.yi@gmail.com>
2024-09-14 15:26:05 +0900

    "code change and add inference"

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -9,8 +9,13 @@ import pandas as pd
 import numpy as np
 import torch.nn as nn
 import os
+from PIL import Image
 
 dataset_down_path = "/Users/seungsulee/Documents/dataset"
+model_save_path = "model_save/mnist_model.pth"
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Anke boot'] ##10가지 분류 레이블
 
 def check_device(str):
     if str == "mac" :
@@ -20,7 +25,7 @@ def check_device(str):
 
     return device
 
-def mnist_init(show_dataset =  1):
+def mnist_init(show_dataset =  0):
     print("---------------------------------------------")
     print("--            FASHION MNIST MODEL          --")
     print("--                    program date: 2024.9 --")
@@ -37,14 +42,12 @@ def mnist_init(show_dataset =  1):
     traindf=datasets.FashionMNIST(root=dataset_down_path,download=True,train=True,transform=transform)
     testdf=datasets.FashionMNIST(root=dataset_down_path,download=True,train=False,transform=transform)
 
-    traindf[0][0].max(),traindf[0][0].min()
-
-    
+    traindf[0][0].max(),traindf[0][0].min()    
 
     idx=np.random.randint(0,len(traindf),25)
     sample=Subset(traindf,idx)
+    test_img = testdf[4][0]
     label_classes=traindf.classes
-
 
     traindf,valdf=random_split(traindf,[len(traindf)-10000,10000])
     len(traindf),len(valdf)
@@ -52,6 +55,14 @@ def mnist_init(show_dataset =  1):
     trainloader=DataLoader(traindf,64,shuffle=True)
     testloader=DataLoader(testdf,64,shuffle=True)
     valloader=DataLoader(valdf,64,shuffle=True)
+
+    #tensor_PIL = transforms.ToPILImage()
+    #image = tensor_PIL(test_img)
+    #image.save("test2.png")
+    #plt.figure(figsize=(1,1))
+    #plt.imshow(image,cmap="gray")
+    #plt.show()   
+    
 
     if show_dataset == 1:
         plt.figure(figsize=(10,8))
@@ -88,7 +99,36 @@ class Model(nn.Module):
         
         # Pass the flattened input through the defined model layers
         return self.model(x)
+
+def inference(mymodel, image_file_path):
+
+    image = Image.open(image_file_path)
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    img_tensor = transform(image)
+
+    device , trainloader, testloader , valloader = mnist_init()
+
+    mymodel.to(device)
+    img_tensor.to(device)
+
+    #### input image debug
+    #tensor_PIL = transforms.ToPILImage()
+    #image = tensor_PIL(img_tensor)
+    #image.save("test.png")
+    #plt.figure(figsize=(1,1))
+    #plt.imshow(image,cmap="gray")
+    #plt.show()
+
+    mps_tensor = img_tensor.to(device)
+
+    pred = mymodel(mps_tensor)
+    cpu_pred = pred.to('cpu')    
+    num_pred = cpu_pred.detach().numpy()
+    #num_pred = np.array(cpu_pred)
+    search_idx = num_pred.argmax()
     
+    return class_names[search_idx]
 
 def train(model, trainloader, loss_fn, optimizer, metric, device):
     # Initialize a list to store the loss for each batch
@@ -170,7 +210,7 @@ def set_loss_func(model, device):
 
     return loss, opt, metric
 
-def mnist_model_proc(mode , epoch_num = 0):
+def mnist_model_proc(mode , epoch_num = 0 , input_file_path = ""):
 
     device , trainloader, testloader , valloader = mnist_init(show_dataset = 0)
 
@@ -184,8 +224,8 @@ def mnist_model_proc(mode , epoch_num = 0):
    
     loss, opt, metric = set_loss_func(mymodel, device)
 
-    if os.path.isfile("model_save/mnist_model.pth"):
-        mymodel = torch.load("model_save/mnist_model.pth")
+    if os.path.isfile(model_save_path):
+        mymodel = torch.load(model_save_path)
 
     if mode == 0 : ## train
         train(mymodel, trainloader, loss, opt, metric, device) # check
@@ -197,19 +237,22 @@ def mnist_model_proc(mode , epoch_num = 0):
     
             print(f"For epoch {i+1}, the training loss is {avg_loss_train} and accuracy is {avg_acc_train}%, "
               f"and for validation, loss is {avg_loss_test} and accuracy is {avg_acc_test}%.")
+        torch.save(mymodel,model_save_path) 
+
+    elif mode == 1 : ## inference
+        result_str = inference(mymodel, input_file_path) 
+
+        print("Inference Result : ", result_str)
+
         
-
-        torch.save(mymodel,"model_save/mnist_model.pth") 
-
-
-
 
 if __name__ == '__main__':
 
-    device , trainloader, testloader , valloader = mnist_init()
+    device , trainloader, testloader , valloader, test_tensor = mnist_init()
 
     mymodel=Model()
-    mymodel.to(device)
+    #mymodel.to(device)
+    mymodel.to('mps')
     mymodel(torch.rand(10,28,28).to(device)).shape
 
     #loss=nn.CrossEntropyLoss()
@@ -218,8 +261,26 @@ if __name__ == '__main__':
    
     loss, opt, metric = set_loss_func(mymodel, device)
 
-    if os.path.isfile("model_save/mnist_model.pth"):
-        mymodel = torch.load("model_save/mnist_model.pth")
+    if os.path.isfile(model_save_path):
+        mymodel = torch.load(model_save_path)
+
+    mymodel.to('mps')
+    tensor_PIL = transforms.ToPILImage()
+    image = tensor_PIL(test_tensor)
+    image.save("test1.png")
+    plt.figure(figsize=(1,1))
+    plt.imshow(image,cmap="gray")
+    plt.show()
+
+    mps_tensor = test_tensor.to(device)
+    pred = inference(mymodel, mps_tensor)
+    cpu_pred = pred.to('cpu')
+    print(cpu_pred)
+    num_pred = cpu_pred.detach().numpy()
+    #num_pred = np.array(cpu_pred)
+    search_idx = num_pred.argmax()
+    print(class_names[search_idx])    
+    
 
     train(mymodel, trainloader, loss, opt, metric, device) # check
     validate(mymodel, valloader, loss, metric, device)    
@@ -233,7 +294,7 @@ if __name__ == '__main__':
               f"and for validation, loss is {avg_loss_test} and accuracy is {avg_acc_test}%.")
         
 
-    torch.save(mymodel,"model_save/mnist_model.pth")
+    torch.save(mymodel,model_save_path)
     
 
 
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     
     if sys.argv[1] == '-h':
         print('arg : -r(run), -m(view moel) -s(show support model)')
-        print("python3 -r [Model] [train :0 , inference :1 ] epoch_count")
+        print("python3 -r [Model] [train :0 , inference :1 ] epoch_count/input_file_path(png)")
     
     if sys.argv[1] == '-m':
         print("1. fashion mnist")
@@ -21,7 +21,9 @@ if __name__ == '__main__':
     if sys.argv[1] == '-r':
         if sys.argv[2] == 'mnist' : ## mnist model
             if sys.argv[3] == '0' : ## train                    
-                mnist_model(0, int(sys.argv[4]))
+                mnist_model(0, epoch_num = int(sys.argv[4]))
+            elif sys.argv[3] == '1' : ## inference
+                mnist_model(1, epoch_num = 0, input_file_path= sys.argv[4])
     
 
 
